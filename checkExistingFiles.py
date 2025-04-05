c09c67935eab599517fea7785f0bb817809f60cb
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-05 13:16:39 +0200

    Create config.py

--- checkExistingFiles.py
+++ checkExistingFiles.py
@@ -1,76 +1,7 @@
-import os
-import json
-import warnings
 import download_video
 import download_gcsv
 from logger import Logger
-
-
-def getValidDevices(device_names: list) -> list:
-    """
-    This function returns the list of valid devices.
-    """
-
-    # Load configuration from config.json
-    config_path = "config.json"
-    with open(config_path, "r") as config_file:
-        config = json.load(config_file)
-
-    allowed = config["allowed_devices"]
-
-    allowedFound = [
-        device for device in device_names if os.path.basename(device) in allowed
-    ]
-
-    if not allowedFound:
-        warnings.warn(
-            f"None of '{device_names}' are not allowed. Files will not be created for this device."
-        )
-
-    return allowedFound
-
-
-def getVideoFolder() -> str:
-    """
-    Get the video folder path from the configuration file.
-    """
-    # Load configuration from config.json
-    config_path = "config.json"
-    with open(config_path, "r") as config_file:
-        config = json.load(config_file)
-
-    # Retrieve video directory from the config file
-    video_dir = config["paths"]["video_dir"]
-
-    return video_dir
-
-
-def getGCSVFolder() -> str:
-    """
-    Get the GCSV folder path from the configuration file.
-    """
-    # Load configuration from config.json
-    config_path = "config.json"
-    with open(config_path, "r") as config_file:
-        config = json.load(config_file)
-
-    # Retrieve GCSV directory from the config file
-    gcsv_dir = config["paths"]["gcsv_dir"]
-
-    return gcsv_dir
-
-
-def createDirectories() -> None:
-    """
-    Create directories for video and GCSV files.
-    """
-    # Retrieve directories from the config file
-    video_dir = getVideoFolder()
-    gcsv_dir = getGCSVFolder()
-
-    # Create directories if they don't exist
-    os.makedirs(video_dir, exist_ok=True)
-    os.makedirs(gcsv_dir, exist_ok=True)
+import config
 
 
 def check_devices_and_file_existance(device_names: list):
@@ -84,12 +15,12 @@ def check_devices_and_file_existance(device_names: list):
         name="CheckFilesLogger", log_file="CheckFiles.log"
     ).get_logger()  # TODO define path
 
-    createDirectories()
+    config.createDirectories()
 
-    device_name = getValidDevices(device_names)[0]
+    device_name = config.getValidDevices(device_names)[0]
 
-    device_video_dir = getVideoFolder()
-    device_gcsv_dir = getGCSVFolder()
+    device_video_dir = config.getVideoFolder()
+    device_gcsv_dir = config.getGCSVFolder()
 
     logger.info(f"Saving videos")
     download_video.run(device_names, device_video_dir)
